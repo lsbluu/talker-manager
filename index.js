58c649f7758e06f4835514954f1e852d7a4c76d4
lsblu <lsblu@hotmail.com>
2022-06-23 10:42:51 -0300

    4 - Adicione as validações para o endpoint /login

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ const validateEmail = (req, res, next) => {
      
    if (password.length < 6) { 
      return res.status(400)
-    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); }
-
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   next();
 };
 
