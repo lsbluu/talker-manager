0c154730d6060ed4904b329b8463d0cde85754d9
lsblu <lsblu@hotmail.com>
2022-06-29 00:11:24 -0300

    lint again

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+
 const talkerPath = './talker.json';
 
 const validateEmail = require('./middlewares/validateEmail');
